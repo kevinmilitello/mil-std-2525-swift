23ec2c67e5f2082e7ae66491cd7acd970e847881
kevin <kevin.f.militello@gmail.com>
2017-08-30 21:28:15 -0400

    initial commit

---
+++ mil-std-2525/enums/StandardIdentity.swift
@@ -0,0 +1,17 @@
+//
+//  StandardIdentity.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 8/30/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+enum StandardIdentity {
+    case Pending
+    case Unknown
+    case AssumedFriend
+    case Friend
+    case Neutral
+    case SuspectJoker
+    case HostileFaker
+}

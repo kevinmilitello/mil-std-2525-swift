20d84bd0afc1344bbe2f07568d7fd7ffe20ba57c
kevin <kevin.f.militello@gmail.com>
2017-08-30 21:30:08 -0400

    initial commit

---
+++ mil-std-2525/enums/Context.swift
@@ -0,0 +1,13 @@
+//
+//  Context.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 8/30/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+enum Context {
+    case Reality
+    case Exercise
+    case Simulation
+}

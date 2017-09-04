91a94c9ed61e87813b6107b5df97ad2f5f4570f9
kevin <kevin.f.militello@gmail.com>
2017-09-04 11:57:24 -0400

    initial commit

---
+++ mil-std-2525/MilStd2525Renderer.swift
@@ -0,0 +1,17 @@
+//
+//  MilStd2525Renderer.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 8/30/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+import UIKit
+
+class MilStd2525Renderer: TacticalGraphicIconRenderer {
+    
+    static func imageFrom(symbolId: String) -> UIImage? {
+        return nil
+    }
+    
+}
---
+++ mil-std-2525/TacticalGraphicIconRenderer.swift
@@ -0,0 +1,13 @@
+//
+//  TacticalGraphicIconRenderer.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 8/30/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+import Foundation
+
+class TacticalGraphicIconRenderer {
+    
+}

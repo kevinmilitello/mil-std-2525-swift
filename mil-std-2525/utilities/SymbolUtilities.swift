b7c44936d66202f930d3b609b987fa5791854dfc
kevin <10521228+kevinmilitello@users.noreply.github.com>
2017-09-11 22:03:47 -0400

    initial commit

---
+++ mil-std-2525/utilities/SymbolUtilities.swift
@@ -0,0 +1,24 @@
+//
+//  SymbolUtilities.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 9/11/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+import Foundation
+
+class SymbolUtilities{
+    
+    static func lineColorOfAffiliation(symbolId: String)->CGColor{
+        return UIColor.black.cgColor
+    }
+    
+    static func fillColorOfAffiliation(symbolId: String)->CGColor{
+        return UIColor.black.cgColor
+    }
+    
+    static func hasDefaultFill(symbolId: String)->Bool{
+        return true
+    }
+}

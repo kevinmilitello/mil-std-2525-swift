a7adf4e215fc148f3cc8cf651b949f93a6c1a7a1
kevin <10521228+kevinmilitello@users.noreply.github.com>
2017-09-11 22:04:25 -0400

    add constructor to set basic properties

--- mil-std-2525/models/MilStdSymbol.swift
+++ mil-std-2525/models/MilStdSymbol.swift
@@ -7,18 +7,21 @@
 //
 
 import Foundation
+import CoreLocation
 
 class MilStdSymbol {
     
     var frame: CGRect?
     var boundary: CGPath?
-    var modifiers: AnyObject?
+    var modifiers: [String:String]?
     var amplifiers: AnyObject?
     var standardIdentity: AnyObject?
     var battleDimension: AnyObject?
     var status: AnyObject?
     var mission: AnyObject?
     var lineWidth: CGFloat?
+    var coordinates: [CLLocationCoordinate2D]?
+    var symbolId: String?
     
     //MARK: Colors
     var iconColor: CGColor?
@@ -26,6 +29,20 @@ class MilStdSymbol {
     var fillColor: CGColor?
     var textColor: CGColor?
     var textBackgroundColor: CGColor?
+    var outlineColor: CGColor?
     
-    
+    init(symbolId: String, coordinates: [CLLocationCoordinate2D]?, modifiers: [String:String]?) {
+        guard let _ = modifiers else{
+            self.modifiers = modifiers
+            return
+        }
+        
+        self.coordinates = coordinates
+        self.symbolId = symbolId
+        self.lineColor = SymbolUtilities.lineColorOfAffiliation(symbolId: symbolId)
+        
+        if SymbolUtilities.hasDefaultFill(symbolId: symbolId) {
+            self.fillColor = SymbolUtilities.fillColorOfAffiliation(symbolId: symbolId)
+        }
+    }
 }

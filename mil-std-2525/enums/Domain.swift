c3bf9df1e5c65e6536937074257ca897f2416179
kevin <kevin.f.militello@gmail.com>
2017-09-11 20:40:39 -0400

    initial commit

---
+++ mil-std-2525/enums/Domain.swift
@@ -0,0 +1,15 @@
+//
+//  Domains.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 9/3/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+enum Domain {
+    case Air
+    case Land
+    case Sea
+    case Space
+    case Cyberspace
+}

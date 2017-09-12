3b581c34bc5a936e088a4c6db33a2dc2c4152b53
kevin <kevin.f.militello@gmail.com>
2017-09-11 20:40:53 -0400

    initial commit

---
+++ mil-std-2525/models/MilStdSymbol.swift
@@ -0,0 +1,31 @@
+//
+//  MilStdSymbol.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 8/30/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+import Foundation
+
+class MilStdSymbol {
+    
+    var frame: CGRect?
+    var boundary: CGPath?
+    var modifiers: AnyObject?
+    var amplifiers: AnyObject?
+    var standardIdentity: AnyObject?
+    var battleDimension: AnyObject?
+    var status: AnyObject?
+    var mission: AnyObject?
+    var lineWidth: CGFloat?
+    
+    //MARK: Colors
+    var iconColor: CGColor?
+    var lineColor: CGColor?
+    var fillColor: CGColor?
+    var textColor: CGColor?
+    var textBackgroundColor: CGColor?
+    
+    
+}

15e5f9e89d632c5d812ca0f123add27c12168a25
kevin <10521228+kevinmilitello@users.noreply.github.com>
2017-09-11 21:38:22 -0400

    initial commit

---
+++ mil-std-2525/enums/Dimension.swift
@@ -0,0 +1,20 @@
+//
+//  Dimension.swift
+//  mil-std-2525
+//
+//  Created by Kevin on 9/11/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+import Foundation
+
+enum Dimension {
+    case Unknown
+    case Space
+    case Air
+    case LandUnit
+    case LandEquipmentAndSeaSurface
+    case LandInstallation
+    case Subsurface
+    case ActivityOrEvent
+}

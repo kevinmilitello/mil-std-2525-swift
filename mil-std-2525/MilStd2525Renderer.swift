499528ed2819ab969d17bf728a381feb4ce2dba9
kevin <10521228+kevinmilitello@users.noreply.github.com>
2017-09-11 22:25:21 -0400

    Add weather color checks

--- mil-std-2525/MilStd2525Renderer.swift
+++ mil-std-2525/MilStd2525Renderer.swift
@@ -19,6 +19,28 @@ class MilStd2525Renderer: TacticalGraphicIconRenderer {
     }
     
     public func iconFrom(symbolId: String, pixelHeightWidth: CGFloat, color: CGColor?) -> UIImage?{
+        var milStdSymbol = MilStdSymbol(symbolId: symbolId, coordinates: nil, modifiers: nil)
+        var id = symbolId
+        if !SymbolUtilities.isWeather(symbolId: id){
+            id = symbolId.substring(to: symbolId.index(after: symbolId.startIndex))
+                .appending("*")
+                .appending(symbolId.substring(with: symbolId.index(symbolId.startIndex, offsetBy: 2)..<symbolId.index(symbolId.startIndex, offsetBy: 3))) //<--Really?
+                .appending("P")
+                .appending(symbolId.substring(to: symbolId.index(symbolId.startIndex, offsetBy: 4)))
+            guard (color != nil) else{
+                return nil
+            }
+            milStdSymbol.lineColor = color
+        }
+        else{
+            var tempColor = SymbolUtilities.fillColorOfWeather(symbolId: symbolId)
+            if tempColor == nil{
+                tempColor = SymbolUtilities.lineColorOfWeather(symbolId: symbolId)
+            }
+            if tempColor != nil{
+                milStdSymbol.lineColor = tempColor
+            }
+        }
         return nil
     }
 }

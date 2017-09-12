e1e1107e2127c5860c2a50a99ea49c08fd4578d1
kevin <kevin.f.militello@gmail.com>
2017-09-11 20:42:45 -0400

    initial commit

---
+++ mil-std-2525/mil_std_2525.h
@@ -0,0 +1,19 @@
+//
+//  mil_std_2525.h
+//  mil-std-2525
+//
+//  Created by Kevin on 8/30/17.
+//  Copyright © 2017 Kevin Militello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mil_std_2525.
+FOUNDATION_EXPORT double mil_std_2525VersionNumber;
+
+//! Project version string for mil_std_2525.
+FOUNDATION_EXPORT const unsigned char mil_std_2525VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mil_std_2525/PublicHeader.h>
+
+
